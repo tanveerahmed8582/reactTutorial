f30132e7aed29dc2620102a93cf4766e670d4936
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2025-09-23 09:17:20 +0530

    Learning useEffect

--- 37 Advanced React/social_media/src/components/PostList.jsx
+++ 37 Advanced React/social_media/src/components/PostList.jsx
@@ -1,25 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
-  const handleGetPostsClick = () => {
-    fetch("https://dummyjson.com/posts")
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    setFetching(true);
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
-      .then((data) => addInitialPosts(data.posts));
-  };
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+      });
+
+    return () => {
+      console.log("cleaning up useEffect");
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
--- 37 Advanced React/social_media/src/components/WelcomeMessage.jsx
+++ 37 Advanced React/social_media/src/components/WelcomeMessage.jsx
@@ -1,14 +1,7 @@
-const WelcomeMessage = ({ onGetPostsClick }) => {
+const WelcomeMessage = () => {
   return (
     <center className="welcome-message">
       <h1>There is no post.</h1>
-      <button
-        type="button"
-        onClick={onGetPostsClick}
-        className="btn btn-success"
-      >
-        Get post from server
-      </button>
     </center>
   );
 };
